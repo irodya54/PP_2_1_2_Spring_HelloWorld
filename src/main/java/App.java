458d20076921b07dabbb914aaeb9d70f137b0f05
Radion Merzkyakov <ya.irodya54@yandex.ru>
2022-07-27 00:55:06 +0300

    finished

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        Cat one =
+                (Cat) applicationContext.getBean("cat");
+        Cat two =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean1);
+        System.out.println(one == two);
     }
 }
